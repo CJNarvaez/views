91d275fd3efd8e1113da21a5f8ca2ec9146cab61
Cristhian Narvaez <cristhiannarvaez@gmail.com>
2015-11-18 15:20:27 -0600

    Graficas
    Se actualizo las fuentes de jquery, highcharts a los CDN y se agrego el
    mapa a los metadatos de los indicadores

--- vw_encabezado.php
+++ vw_encabezado.php
@@ -22,12 +22,15 @@ background-repeat: no-repeat;
     }
 </style>
 <!-- Bootstrap core CSS -->
-<link href="<?php echo base_url('/css/bootstrap.css') ?>" rel="stylesheet" />
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+
 <!--
 <link type="text/css" href="<?php// echo base_url('css/ssz/jquery-ui-1.10.3.custom.css'); ?>" rel="Stylesheet" />  
 <script type="text/javascript" src="<?php// echo base_url('js/jquery-ui-1.10.3.custom.min.js') ?>"></script> 
 <link href="<?php //echo base_url('/css/web20.css') ?>" rel="stylesheet" type="text/css" />-->
-<script type="text/javascript" src="<?php echo base_url('js/jquery-2.0.3.js') ?>"></script>
-<script src="<?php echo base_url('js/bootstrap.min.js') ?>"></script>
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js" integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ==" crossorigin="anonymous"></script>
 </head>
 <body>
